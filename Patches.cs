106d30d73ca2fe50ea68b40840dd249b0d6951f2
Zinal001 <Zinal001@users.noreply.github.com>
2022-09-25 09:03:10 +0200

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tech.zinals.valheim.shieldautoequip
+{
+    internal static class Patches
+    {
+        internal static HarmonyLib.Harmony HarmonyInstance { get; private set; }
+
+        internal static void Patch()
+        {
+            HarmonyInstance = new HarmonyLib.Harmony("tech.zinals.valheim.shieldautoequip");
+            HarmonyInstance.PatchAll(typeof(Patches));
+        }
+
+
+        [HarmonyLib.HarmonyPatch(typeof(Humanoid), nameof(Humanoid.EquipItem)), HarmonyLib.HarmonyPostfix()]
+        private static void Humanoid_EquipItem_Postfix(Humanoid __instance, ItemDrop.ItemData item, ref bool __result, ItemDrop.ItemData ___m_leftItem)
+        {
+            if(__result && item.m_shared.m_itemType == ItemDrop.ItemData.ItemType.OneHandedWeapon && ___m_leftItem == null)
+            {
+                if (Plugin.DetectedWeapons.Any() && !Plugin.DetectedWeapons.Contains(item.m_shared.m_name))
+                    return;
+
+                List<ItemDrop.ItemData> shieldItems = new List<ItemDrop.ItemData>();
+                __instance.GetInventory().GetAllItems(ItemDrop.ItemData.ItemType.Shield, shieldItems);
+                shieldItems = shieldItems.Where(s => (!s.m_shared.m_useDurability || s.m_durability > 0f) && (!Plugin.EquippableShields.Any() || Plugin.EquippableShields.Contains(s.m_shared.m_name))).OrderByDescending(s => s.GetBlockPower(__instance.GetSkillFactor(Skills.SkillType.Blocking))).ThenByDescending(s => s.m_durability).ToList();
+                if (shieldItems.Any())
+                {
+                    Plugin.PluginLogger.LogInfo($"Equipping shield {shieldItems[0].m_shared.m_description}");
+                    __instance.EquipItem(shieldItems[0], true);
+                }
+            }
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using System.Linq;
+
+namespace tech.zinals.valheim.shieldautoequip;
+
+[BepInPlugin("tech.zinals.valheim.shieldautoequip", "Shield Auto-Equip", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static BepInEx.Logging.ManualLogSource PluginLogger { get; private set; }
+
+    private BepInEx.Configuration.ConfigEntry<string> _DetectedWeapons;
+    private BepInEx.Configuration.ConfigEntry<string> _EquippableShields;
+
+    internal static string[] DetectedWeapons { get; private set; }
+    internal static string[] EquippableShields { get; private set; }
+
+    private void Awake()
+    {
+        PluginLogger = Logger;
+
+        SetupConfig();
+        Patches.Patch();
+
+        Logger.LogInfo($"Plugin Shield Auto-Equip (1.0.0) is loaded!");
+    }
+
+    #region Configuration
+    private void SetupConfig()
+    {
+        _DetectedWeapons = Config.Bind("Common", "Detected Weapons", "", "Weapons/Tools that should equip a shield. Separated by comma (,). Leave empty to allow all one-handed weapons.\nExample: MaceBronze, KnifeFlint, Club");
+        _EquippableShields = Config.Bind("Common", "Equippable Shields", "", "Allowed shield types to auto-equip. Separated by comma (,). Leave empty to allow all shields.\nExample: ShieldWood, ShieldBronzeBuckler, ShieldBanded");
+
+        Config.ConfigReloaded += Config_ConfigReloaded;
+        Config_ConfigReloaded(null, null);
+    }
+
+    private void Config_ConfigReloaded(object sender, System.EventArgs e)
+    {
+        if (string.IsNullOrEmpty(_DetectedWeapons.Value))
+            DetectedWeapons = new string[0];
+        else
+            DetectedWeapons = _DetectedWeapons.Value.Split(',').Select(s => s.Trim()).ToArray();
+
+        if (string.IsNullOrEmpty(_EquippableShields.Value))
+            EquippableShields = new string[0];
+        else
+            EquippableShields = _EquippableShields.Value.Split(',').Select(s => s.Trim()).ToArray();
+    }
+    #endregion
+}
